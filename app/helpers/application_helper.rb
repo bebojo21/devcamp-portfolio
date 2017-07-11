f8967d5ee2e2923a0ec7a5d94856d8a8b897120a
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-11 17:28:08 +0000

    Implemented authorization engine for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
     def login_helper
-        if current_user.is_a?(User)
-            link_to "Logout", destroy_user_session_path, method: :delete
-        else
+        if current_user.is_a?(GuestUser)
             (link_to "Register", new_user_registration_path) +
             "<br>".html_safe +
             (link_to "Login", new_user_session_path)
+        else
+            link_to "Logout", destroy_user_session_path, method: :delete
         end
     end
     
@@ -17,6 +17,6 @@ module ApplicationHelper
     end
     
     def copyright_generator
-        @copyright = IsaacJohnsenViewTool::Renderer.copyright "Isaac Johnsen", "All rights reserved"
+        IsaacJohnsenViewTool::Renderer.copyright "Isaac Johnsen", "All rights reserved"
     end
 end
