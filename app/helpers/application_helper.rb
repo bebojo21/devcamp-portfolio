9a3cec48ce5ef7beef4b199600ac4ff52b8c9839
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-13 14:47:21 +0000

    Updated styles and added map for contact page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-    def login_helper style
+    def login_helper style = ''
         if current_user.is_a?(GuestUser)
             (link_to "Register", new_user_registration_path, class: style).html_safe +
             (link_to "Login", new_user_session_path, class: style)
@@ -16,6 +16,6 @@ module ApplicationHelper
     end
     
     def copyright_generator
-        IsaacJohnsenViewTool::Renderer.copyright "Isaac Johnsen", "All rights reserved"
+        @copyright = IsaacJohnsenViewTool::Renderer.copyright "Isaac Johnsen", "All rights reserved"
     end
 end
