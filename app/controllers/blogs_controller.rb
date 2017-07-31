f94646b72312de0d5849b191ad6867ac22f25de3
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-31 03:13:20 +0000

    Implemented redirect for blog show page when non-admin tries to access draft blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,11 +13,15 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
-    
-    @page_title = @blog.title
-    @seo_keywords = @blog.title
+    if logged_in?(:site_admin) || @blog.published?
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
+      
+      @page_title = @blog.title
+      @seo_keywords = @blog.title
+    else
+      redirect_to blogs_path, notice: "You are not authorized to access this page."
+    end
   end
 
   def new
